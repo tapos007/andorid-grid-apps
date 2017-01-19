55f2101924128b2f04c075cd90fb532498224d0b
tapos <tapos.aa@gmail.com>
2017-01-19 13:26:17 +0600

    grid view with sound play work done

---
+++ app/src/main/java/com/example/andttapos/basicphrases/MainActivity.java
@@ -0,0 +1,29 @@
+package com.example.andttapos.basicphrases;
+
+import android.media.MediaPlayer;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+
+    public void buttonTapped(View view) {
+        int id = view.getId();
+        String ourId = "";
+        ourId = view.getResources().getResourceEntryName(id);
+
+        int resourceId = getResources().getIdentifier(ourId,"raw","com.example.andttapos.basicphrases");
+
+        MediaPlayer mPlayer = MediaPlayer.create(this,resourceId );
+        mPlayer.start();
+        Log.i("button tapped", ourId);
+    }
+}
